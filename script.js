2d5f259d34e0ee44fe75fe38740c9d6999c5f10f
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-03-27 19:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function threeSum(arr, target) {
-var sum=0;
+	let min=Number.MAX_VALUE;
+var ans=0;
 	for(var i=0;i<arr.length;i++){
-		sum=sum+arr[i];
-		if(sum>=target){
-			return sum;
+		for( var j=i+1;j<arr.length;j++){
+			for( var k=j+1;k<arr.length;k++){
+				let sum=arr[i]+arr[j]+arr[k];
+				if(Math.abs(sum-targer)<=min){
+					min=Math.abs(sum-target);
+					ans=sum;
+				}
+				
+			}
 		}
 	}
-  return sum;
+  return ans;
 }
 
 module.exports = threeSum;
